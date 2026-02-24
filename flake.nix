47184f121c6936eb2c8ded0afb83a21c4a31dc2e
Jul Lang <jullanggit@proton.me>
2026-02-24 09:02:00 +0100

    Initial

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "Dashboard Devshell";
+
+  inputs = {
+    host.url = "path:/etc/nixos";
+
+    nixpkgs.follows = "host/nixpkgs";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        };
+
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          targets = [ "wasm32-unknown-unknown" ];
+          extensions = [
+            "rust-src"
+            "rust-analyzer"
+          ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            rustToolchain
+            dioxus-cli
+            vtsls
+            eslint
+            sqlite
+            sea-orm-cli
+            swc
+          ];
+        };
+      }
+    );
+}
