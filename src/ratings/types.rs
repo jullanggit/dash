db365ad6a77487c72ece0d7037198d9c086a4b93
Jul Lang <jullanggit@proton.me>
2026-03-05 18:13:28 +0100

    use structstruck for rating types

--- src/ratings/types.rs
+++ src/ratings/types.rs
@@ -6,60 +6,45 @@ use time::UtcDateTime;
 type RatingCategory = String;
 
 pub type Data = HashMap<RatingCategory, Vec<Rating>>;
-
-#[derive(Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Rating {
-    pub uid: String,
-    pub added_at: String,
-    pub added_by: AddedBy,
-    pub uri: Uri,
-    pub name: String,
-    pub album: Album,
-    pub artists: Vec<Artist>,
-    pub disc_number: u16,
-    pub track_number: u16,
-    pub duration: TrackDuration,
-    pub is_explicit: bool,
-    pub is_local: bool,
-    pub is_playable: bool,
-    pub is19_plus_only: bool,
-    pub has_associated_video: bool,
-    pub has_associated_audio: bool,
-    pub is_banned: bool,
-}
-
 type Uri = String;
 
-#[derive(Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct AddedBy {
-    pub uri: Uri,
-    pub username: String,
-    pub display_name: String,
-}
-
-#[derive(Deserialize)]
-pub struct Album {
-    pub uri: Uri,
-    pub name: String,
-    pub artist: Artist,
-    pub images: Vec<Image>,
-}
-
-#[derive(Deserialize)]
-pub struct Image {
-    pub url: String, // not an http(s) url
-    pub label: String,
-}
-
-#[derive(Deserialize)]
-pub struct Artist {
-    pub uri: Uri,
-    pub name: String,
-}
-
-#[derive(Deserialize)]
-pub struct TrackDuration {
-    pub milliseconds: u64,
-}
+structstruck::strike!(
+    #[structstruck::each[derive(Debug, Deserialize)]]
+    #[structstruck::each[serde(rename_all = "camelCase")]]
+    pub struct Rating {
+        pub uid: String,
+        pub added_at: String,
+        pub added_by: struct {
+            pub uri: Uri,
+            pub username: String,
+            pub display_name: String,
+        },
+        pub uri: Uri,
+        pub name: String,
+        pub album: struct {
+            pub uri: Uri,
+            pub name: String,
+            pub artist: Artist,
+            pub images: Vec<struct Image {
+                pub url: String, // not an http(s) url
+                pub label: String,
+            }>,
+        },
+        pub artists: Vec<struct Artist {
+            pub uri: Uri,
+            pub name: String,
+        }>,
+        pub disc_number: u16,
+        pub track_number: u16,
+        pub duration: struct TrackDuration {
+            pub milliseconds: u64,
+        },
+        pub is_explicit: bool,
+        pub is_local: bool,
+        pub is_playable: bool,
+        pub is19_plus_only: bool,
+        pub has_associated_video: bool,
+        pub has_associated_audio: bool,
+        pub is_banned: bool,
+    }
+);
