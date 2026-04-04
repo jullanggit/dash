ec3723624a5228e642e270246f95b5631089178d
Jul Lang <jullanggit@proton.me>
2026-04-04 21:00:54 +0200

    make password-hash binary read from stdin instead of env::args

--- src/bin/password-hash.rs
+++ src/bin/password-hash.rs
@@ -1,3 +1,5 @@
+use std::io::stdin;
+
 use argon2::{
     Argon2,
     password_hash::{PasswordHasher, SaltString},
@@ -5,20 +7,13 @@ use argon2::{
 use rand_core::OsRng;
 
 fn main() {
-    let mut args = std::env::args();
-    let program = args.next().unwrap_or_else(|| "password-hash".to_string());
+    let mut password = String::new();
+    stdin().read_line(&mut password).unwrap();
 
-    if let Some(password) = args.next()
-        && args.next().is_none()
-    {
-        let salt = SaltString::generate(&mut OsRng);
-        let hash = Argon2::default()
-            .hash_password(password.as_bytes(), &salt)
-            .expect("password hashing should succeed");
+    let salt = SaltString::generate(&mut OsRng);
+    let hash = Argon2::default()
+        .hash_password(password.as_bytes(), &salt)
+        .expect("password hashing should succeed");
 
-        println!("{hash}");
-    } else {
-        eprintln!("usage: {program} <password>");
-        std::process::exit(2);
-    };
+    println!("{hash}");
 }
